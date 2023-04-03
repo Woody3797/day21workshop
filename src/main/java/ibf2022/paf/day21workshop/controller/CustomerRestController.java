ee67bf295353bedac580ece311aa140dec0f2ccf
Woody3797 <woody3797@gmail.com>
2023-04-03 21:59:08 +0800

    day21workshop

--- src/main/java/ibf2022/paf/day21workshop/controller/CustomerRestController.java
+++ src/main/java/ibf2022/paf/day21workshop/controller/CustomerRestController.java
@@ -1,5 +1,6 @@
 package ibf2022.paf.day21workshop.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -7,28 +8,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ibf2022.paf.day21workshop.model.Customer;
+import ibf2022.paf.day21workshop.model.Order;
 import ibf2022.paf.day21workshop.respository.CustomerRepository;
 import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
 
 @RestController
 @RequestMapping(path = "/api/customers", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CustomerRestController {
 
-    CustomerRepository customerRepository;
+    public CustomerRepository customerRepository;
 
     public CustomerRestController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     @GetMapping
-    public ResponseEntity<String> getallCustomers(@RequestParam(required = false) String offset, @RequestParam(required = false) String limit) {
+    public ResponseEntity<String> getallCustomers(@RequestParam(required = false) String offset,
+            @RequestParam(required = false) String limit) {
 
         if (Objects.isNull(offset)) {
             offset = "0";
@@ -37,7 +43,8 @@ public class CustomerRestController {
             limit = "5";
         }
 
-        List<Customer> customers = customerRepository.getAllCustomers(Integer.parseInt(offset), Integer.parseInt(limit));
+        List<Customer> customers = customerRepository.getAllCustomers(Integer.parseInt(offset),
+                Integer.parseInt(limit));
 
         JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
 
@@ -48,4 +55,38 @@ public class CustomerRestController {
 
         return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(result.toString());
     }
+
+    @GetMapping(path = "{customerId}")
+    public ResponseEntity<String> getCustomerById(@PathVariable Integer customerId) {
+        JsonObject result = null;
+        try {
+            Customer customer = customerRepository.findCustomerById(customerId);
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("customer", customer.toJson());
+            result = objectBuilder.build();
+        } catch (IndexOutOfBoundsException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON)
+                    .body("{\"error_msg\": \"record not found\"}");
+        }
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(result.toString());
+    }
+
+    @GetMapping(path = "{customer_id}/orders")
+    public ResponseEntity<String> getOrdersFromCustomer(@PathVariable Integer customer_id) {
+
+        List<Order> orders = new ArrayList<>();
+        JsonArray result = null;
+        orders = customerRepository.getCustomerOrders(customer_id);
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        for (Order o : orders) {
+            arrayBuilder.add(o.toJson());
+        }
+        result = arrayBuilder.build();
+        if (result.size() == 0) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON)
+                    .body("{\"error_msg\": \"record not found\"}");
+        }
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(result.toString());
+    }
+
 }
--- src/main/java/ibf2022/paf/day21workshop/respository/CustomerRepository.java
+++ src/main/java/ibf2022/paf/day21workshop/respository/CustomerRepository.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
 import ibf2022.paf.day21workshop.model.Customer;
+import ibf2022.paf.day21workshop.model.Order;
 
 @Repository
 public class CustomerRepository {
@@ -24,6 +25,25 @@ public class CustomerRepository {
         while (rs.next()) {
             customers.add(Customer.create(rs));
         }
-        return null;
+        return customers;
     }
+
+    // fetch customer using id
+    public Customer findCustomerById(Integer id) {
+        List<Customer> customers = template.query(DBQueries.SELECT_CUSTOMER_BY_ID, new CustomerRowMapper(), new Object[]{id});
+
+        return customers.get(0);
+    }
+
+    // fetch orders from customer
+    public List<Order> getCustomerOrders(Integer id) {
+        List<Order> orders = new ArrayList<>();
+        SqlRowSet rs = template.queryForRowSet(DBQueries.SELECT_ORDERS_FROM_CUSTOMER, new Object[]{id});
+
+        while (rs.next()) {
+            orders.add(Order.create(rs));
+        }
+        return orders;
+    }
+
 }
